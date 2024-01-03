a06a53c66977f750f4aa939ac7dd26b1c85ef10d
ajmecca68 <137560371+ajmecca68@users.noreply.github.com>
2024-01-02 22:08:35 -0500

    modify server.js to accomodate apolloserver start

--- server/server.js
+++ server/server.js
@@ -4,44 +4,43 @@ const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const AuthService = require('./utils/auth');
 
-// Import your type definitions and resolvers
-const typeDefs = require('./schemas/typeDefs'); 
-const resolvers = require('./schemas/resolvers'); 
+const typeDefs = require('./schemas/typeDefs');
+const resolvers = require('./schemas/resolvers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Apollo Server
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => {
-    // Use the authenticate method from AuthService to check user credentials
-    const authService = new AuthService();
-    try {
-      authService.authenticateGraphQL(req);
-      return { user: req.user };
-    } catch (error) {
-      console.error(error);
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+      // Use the authenticate method from AuthService to check user credentials
+      const authService = new AuthService();
+      try {
+        authService.authenticateGraphQL(req);
+        return { user: req.user };
+      } catch (error) {
+        console.error(error);
+      }
     }
+  });
+
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
   }
-});
 
-// Apply Apollo GraphQL middleware and set the path to /graphql
-server.applyMiddleware({ app, path: '/graphql' });
+  // Add your other routes here
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// Serve static assets if in production environment
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
 }
 
-// Add routes
-app.use(routes);
-
-// Connect to the database
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+startApolloServer(typeDefs, resolvers);
