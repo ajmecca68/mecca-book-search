274e717fbc5360da6d12dab795b6bd9fd8d6f9db
ajmecca68 <137560371+ajmecca68@users.noreply.github.com>
2024-01-09 19:30:09 -0500

    updated mongo string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://amecca1968:Password1234@cluster0.nqnpz74.mongodb.net/');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://amecca1968:Password1234@cluster0.nqnpz74.mongodb.net/mecca-books');
 
 module.exports = mongoose.connection;
